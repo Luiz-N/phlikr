0ffb15d3e1d22683e8934de68163f9c3f5161160
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-05 15:39:07 -0500

    new user creation

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -9,7 +9,56 @@ end
 
 
 get '/new_album' do
+  if current_user
+    erb :new_album
+  else
+    erb :login
+  end
+end
+
+get '/login' do
+
+  erb :login
+end
+
+get '/new_user' do
+
+  erb :new_user
+end
+
+
+
+
+##############POST##########################
+
+post '/user/login' do
+
+  if User.find_by_email(params[:email])
+    @login = User.find_by_email(params[:email])
+    if @login.password == params[:password]
+      session[:user_id] = @login.id
+      redirect "/"
+    else
+      @error = "Incorrect email and/or password"
+      erb :login #email or pw incorrect
+      # redirect "/user/login"
+    end
+  else
+    @error = "Incorrect email and/or password"
+    erb :login #error no user message
+  end
+
+end
+
+post '/user/new' do
+  @user = User.find_by_email(params[:user][:email])
   
+  # If they don't exist, create new user
+  if @user.nil? 
+    @user = User.create(params[:user])
+    session[:user_id] = @user.id
 
+  end
 
+  redirect "/"
 end
